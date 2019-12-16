270d0326ae7c90f0fcb515027daedb96856cec9f
Phuong Minh <phuongminh2303@gmail.com>
2019-12-16 09:45:21 +0700

    Update history, admins, reviews, newlocations schemas

--- db_schema.sql
+++ db_schema.sql
@@ -20,3 +20,45 @@ CREATE TABLE clients(
   phone VARCHAR(20),
   FOREIGN KEY (client_id) REFERENCES registration(id)
 );
+
+CREATE TABLE admins(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE newlocations(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(50),
+  latitude DECIMAL(20,10),
+  longitude DECIMAL(20,10),
+  openTime TIME,
+  closeTime TIME,
+  phone VARCHAR(20),
+  email VARCHAR(50),
+  type VARCHAR(20),
+  rating DECIMAL(3,2),
+  price DECIMAL(20,10),
+  url VARCHAR(50)
+);
+
+CREATE TABLE reviews(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  client_id INT NOT NULL,
+  location_id INT,
+  created_at DATETIME,
+  content TEXT,
+  rating FLOAT(3, 1),
+  FOREIGN KEY (client_id) REFERENCES registration(id),
+  FOREIGN KEY (location_id) REFERENCES newlocations(id)
+);
+
+CREATE TABLE history(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  client_id INT NOT NULL,
+  latitude DECIMAL(20,10),
+  longitude DECIMAL(20,10),
+  arrival_at DATETIME,
+  leave_at DATETIME
+);
