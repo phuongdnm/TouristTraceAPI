64167224e9ec4b9347bfd3ce7fcdaf50293d01d7
Phuong Minh <phuongminh2303@gmail.com>
2019-12-18 23:32:39 +0700

    Add config for production deploy

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { createPool } = require('mysql');
 
 const pool = createPool({
-  port: process.env.DB_PORT,
+  port: '/var/run/mysqld/mysqld.sock',
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const userRouter = require('./api/users/user.router');
 
-const app  = express();
+const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+ 
+const port = (process.env.NODE_ENV === 'production') ? process.env.PORT : 8080;
 
-const port = process.env.PORT | 8080;
 app.listen(port, () => {
   console.log('Server is running on port ', port);
-})
+});
